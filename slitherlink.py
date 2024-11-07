558df902048b06cb83dff708b7fc595c28a53584
darilin98 <darekrudis@email.cz>
2024-11-07 22:27:44 +0100

    Rename script file

---
+++ slitherlink.py
@@ -0,0 +1,112 @@
+import subprocess
+from argparse import ArgumentParser
+
+def load_instance(input_file_name):
+ 
+    return 
+
+def encode(instance):
+    # given the instance, create a cnf formula, 
+    # i.e. a list of lists of integers
+    # also return the total number of variables used
+    return
+ 
+def call_solver(cnf, nr_vars, output_name, solver_name, verbosity):
+    # print CNF into formula.cnf in DIMACS format
+    with open(output_name, "w") as file:
+        file.write("p cnf " + str(nr_vars) + " " + str(len(cnf)) + '\n')
+        for clause in cnf:
+            file.write(' '.join(str(lit) for lit in clause) + '\n')
+
+    # call the solver and return the output
+    return subprocess.run(['./' + solver_name, '-model', '-verb=' + str(verbosity) , output_name], stdout=subprocess.PIPE)
+
+def print_result(result):
+    for line in result.stdout.decode('utf-8').split('\n'):
+        print(line)     # print the whole output of the SAT solver to stdout, so you can see the raw output for yourself
+
+    # check the returned result
+    if (result.returncode == 20):   # returncode for SAT is 10, for UNSAT is 20
+        return
+
+    # parse the model from the output of the solver
+    # the model starts with 'v'
+    model = []
+    for line in result.stdout.decode('utf-8').split('\n'):
+        if line.startswith("v"):    # there might be more lines of the model, each starting with 'v'
+            vars = line.split(" ")
+            vars.remove("v")
+            model.extend(int(v) for v in vars)      
+    model.remove(0) # 0 is the end of the model, just ignore it
+
+    print()
+    print("##################################################################")
+    print("###########[ Human readable result of the tile puzzle ]###########")
+    print("##################################################################")
+    print()
+
+    # decode the meaning of the assignment
+    for t in range(MAKESPAN+1):
+        print("Timestep " + str(t) + ":")
+        for p in range(NR_TILES):
+            for n in range(NR_TILES):
+                if model[pos_to_atID(n,p,t) - 1] > 0:
+                    print(n, end=" ")
+            if (p+1) % GRID_SIZE == 0:
+                print()
+        print()
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "-i",
+        "--input",
+        default="input.in",
+        type=str,
+        help=(
+            "The instance file."
+        ),
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default="formula.cnf",
+        type=str,
+        help=(
+            "Output file for the DIMACS format (i.e. the CNF formula)."
+        ),
+    )
+    parser.add_argument(
+        "-s",
+        "--solver",
+        default="glucose-syrup",
+        type=str,
+        help=(
+            "The SAT solver to be used."
+        ),
+    )
+    parser.add_argument(
+        "-v",
+        "--verb",
+        default=1,
+        type=int,
+        choices=range(0,2),
+        help=(
+            "Verbosity of the SAT solver used."
+        ),
+    )
+    args = parser.parse_args()
+
+    # get the input instance
+    instance = load_instance(args.input)
+
+    # encode the problem to create CNF formula
+    cnf, nr_vars = encode(instance)
+
+    # call the SAT solver and get the result
+    result = call_solver(cnf, nr_vars, args.output, args.solver, args.verb)
+
+    # interpret the result and print it in a human-readable format
+    print_result(result)
