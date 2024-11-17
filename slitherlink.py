65a824d97fae64e60f27712c1a00666adf08f408
darilin98 <darekrudis@email.cz>
2024-11-17 17:10:31 +0100

    Implement working SAT encode on compact boards

--- slitherlink.py
+++ slitherlink.py
@@ -2,18 +2,206 @@ import subprocess
 from argparse import ArgumentParser
 
 def load_instance(input_file_name):
- 
-    return 
+    
+    board = []
+    with open(input_file_name, "r") as input:
+        global GRID_LENGTH, GRID_HEIGTH, EDGE_COUNT, ID_OFFSET
+        # Load cell constraint values
+        for line in input:
+            elements = line.split()
+            board.append([int(x) if x!= '.' else None for x in elements])
 
-def encode(instance):
+
+    GRID_HEIGTH = len(board)
+    GRID_LENGTH = len(board[0]) if GRID_HEIGTH > 0 else 0
+
+    # total number of edges formula
+    EDGE_COUNT = GRID_HEIGTH * (GRID_LENGTH + 1) + GRID_LENGTH * (GRID_HEIGTH + 1)
+   
+    ID_OFFSET = EDGE_COUNT + 1 
+
+    return board
+
+def encode(instance): 
     # given the instance, create a cnf formula, 
     # i.e. a list of lists of integers
     # also return the total number of variables used
-    return
- 
+
+    cnf = []
+    # every edge holds information if it exists and if it is reachable
+    variable_count = EDGE_COUNT
+    
+    def generate_cell_one(e1, e2, e3, e4):
+        result = []
+        edges = [e1, e2, e3, e4]
+        #At leaast one is true
+        result.append([e1, e2, e3, e4, 0])
+        
+        #At most one is true
+        for i in range(4):
+            for j in range(i + 1, 4):
+                result.append([-edges[i], -edges[j], 0])
+
+        return result
+
+    def generate_cell_two(e1, e2, e3, e4):
+        result = []
+
+        #At least two are true
+        result.append([e1,e2,e3,e4,0])
+        for i in range(4):
+            clause = [e1, e2, e3, e4, 0]
+            clause[i] = -clause[i]
+            result.append(clause)
+        #At most two are true
+        result.append([-e1, -e2, -e3, 0])
+        result.append([-e1, -e2, -e4, 0])
+        result.append([-e1, -e3, -e4, 0])
+        result.append([-e2, -e3, -e4, 0])
+        
+        return result
+
+    def generate_cell_three(e1, e2, e3, e4):
+        result = []
+        edges = [e1,e2,e3,e4]
+        
+        #At least one is false
+        result.append([-e1,-e2,-e3,-e4,0])
+
+        #At most one is false
+        for i in range(4):
+            for j in range(i + 1, 4):
+                result.append([edges[i], edges[j], 0])
+
+        return result
+
+
+        
+    # generate cell constraints from input
+    for y in range(GRID_HEIGTH):
+        for x in range(GRID_LENGTH):
+            cell_constraint = instance[y][x]
+            combinations = [[]]
+            if cell_constraint == 0:
+                combinations = [[-edge_ID(x,y,"H"), 0], [-edge_ID(x,y,"V"), 0], [-edge_ID(x,y+1,"H"), 0], [-edge_ID(x+1,y,"V"), 0]]
+            elif cell_constraint == 1:
+                combinations = generate_cell_one(edge_ID(x,y,"H"), edge_ID(x,y,"V"), edge_ID(x,y+1,"H"), edge_ID(x+1,y,"V"))
+            elif cell_constraint == 2:
+                combinations = generate_cell_two(edge_ID(x,y,"H"), edge_ID(x,y,"V"), edge_ID(x,y+1,"H"), edge_ID(x+1,y,"V"))
+            elif cell_constraint == 3:
+                combinations = generate_cell_three(edge_ID(x,y,"H"), edge_ID(x,y,"V"), edge_ID(x,y+1,"H"), edge_ID(x+1,y,"V"))
+
+            if len(combinations[0]) != 0:
+                for combination in combinations:
+                    cnf.append(combination)
+
+
+    def add_loop_constraints(x,y,orientation):
+        
+        edge = edge_ID(x,y,orientation)
+
+        a1, a2, a3, b1, b2, b3 = [0] * 6
+
+        if orientation == "V":
+            #Top neighbour
+            if is_within_range(x, y, "H"):
+                a1 = edge_ID(x, y, "H")
+            if is_within_range(x, y-1, "V"):
+                a2 = edge_ID(x, y-1, "V")
+            if is_within_range(x-1, y, "H"):
+                a3 = edge_ID(x-1, y, "H")
+
+            #Bottom neighbours
+            if is_within_range(x, y + 1, "H"):
+                b1 = edge_ID(x, y + 1, "H")
+            if is_within_range(x, y + 1, "V"):
+                b2 = edge_ID(x, y + 1, "V")
+            if is_within_range(x - 1, y + 1, "H"):
+                b3 = edge_ID(x - 1, y + 1, "H")        
+
+        elif orientation == "H":
+            # Left neighbors
+            if is_within_range(x, y,"V"):
+                a1 = edge_ID(x, y, "V")
+            if is_within_range(x - 1, y, "H"):
+                a2 = edge_ID(x - 1, y, "H")
+            if is_within_range(x, y - 1, "V"):
+                a3 = edge_ID(x, y - 1, "V")
+
+            # Right neighbors
+            if is_within_range(x + 1, y - 1, "V"):
+                b1 = edge_ID(x + 1, y - 1, "V")
+            if is_within_range(x + 1, y, "H"):
+                b2 = edge_ID(x + 1, y, "H")
+            if is_within_range(x + 1, y, "V"):
+                b3 = edge_ID(x + 1, y, "V")
+
+        #For an edge to be true, exactly one neighbour from side a and 
+        #exactly one neighbour from side b bust be true
+        clauses = []
+        
+        def generate_neigbour_clauses(e, n1, n2, n3):
+            result_clauses = []
+            # Filter out undefined edges (neighbors represented as 0)
+            neighbors = [n for n in [n1, n2, n3] if n != 0]
+
+            # At least one neighbor is true if the edge is active
+            result_clauses.append([-e] + neighbors)
+
+            # At most one neighbor is true if the edge is active
+            for i in range(len(neighbors)):
+                for j in range(i + 1, len(neighbors)):
+                    result_clauses.append([-e, -neighbors[i], -neighbors[j]])
+
+            cnf_form = []
+            for row in result_clauses:
+                cnf_form.append(row + [0])
+            
+            return cnf_form
+
+        for clause in generate_neigbour_clauses(edge, a1, a2, a3):
+            clauses.append(clause)
+        
+        for clause in generate_neigbour_clauses(edge, b1, b2, b3):
+            clauses.append(clause)
+        
+                       
+        return clauses
+
+    #generate simple loop constraint - every edge has either 1 neighbour on each side and is true
+    #or is false
+    for y in range(GRID_HEIGTH + 1):
+        for x in range(GRID_LENGTH + 1):
+            if x < GRID_LENGTH:
+                for clause in add_loop_constraints(x,y,"H"):
+                    cnf.append(clause)
+            if y < GRID_HEIGTH:
+                for clause in add_loop_constraints(x,y,"V"):
+                    cnf.append(clause)
+
+    return cnf, variable_count
+
+def is_within_range(x, y, orientation):
+    if (x < 0) or (y < 0):
+        return False
+    if orientation == "H":
+        return (1 <= edge_ID(x,y,orientation) <= GRID_LENGTH * (GRID_HEIGTH + 1))
+    if orientation == "V":
+        return ((GRID_LENGTH * (GRID_HEIGTH + 1) + 1) <= edge_ID(x,y,orientation) < ID_OFFSET)
+    
+def edge_ID(x, y, orientation):
+    if orientation == "H":
+        return x * (GRID_HEIGTH + 1) + y + 1
+    elif orientation == "V":
+        offset = GRID_LENGTH * (GRID_HEIGTH + 1)
+        return offset + x + (y * (GRID_LENGTH + 1)) + 1
+    return 
+
 def call_solver(cnf, nr_vars, output_name, solver_name, verbosity):
     # print CNF into formula.cnf in DIMACS format
     with open(output_name, "w") as file:
+        # for row in cnf:
+        #     print(row)
         file.write("p cnf " + str(nr_vars) + " " + str(len(cnf)) + '\n')
         for clause in cnf:
             file.write(' '.join(str(lit) for lit in clause) + '\n')
@@ -38,23 +226,44 @@ def print_result(result):
             vars.remove("v")
             model.extend(int(v) for v in vars)      
     model.remove(0) # 0 is the end of the model, just ignore it
+    
+    print(model)
+    
+    vertical_index = GRID_LENGTH * (GRID_HEIGTH + 1)
+    horizontal_index = 0
+    offset = GRID_LENGTH * (GRID_HEIGTH + 1)
+    
+    grid = [['' for _ in range(GRID_LENGTH + 1)] for _ in range(GRID_LENGTH + GRID_HEIGTH + 1)]
 
-    print()
-    print("##################################################################")
-    print("###########[ Human readable result of the tile puzzle ]###########")
-    print("##################################################################")
-    print()
+    for row_index in range(GRID_LENGTH + GRID_HEIGTH + 1):
+        if row_index % 2 == 0:
+            horizontal_index = row_index // 2
+            for i in range(GRID_LENGTH):
+                if model[horizontal_index] > 0:
+                    grid[row_index][i+1] = "-----"
+                else:
+                    grid[row_index][i+1] = "     "
+                horizontal_index += GRID_HEIGTH + 1
+        else:
+            for j in range(GRID_LENGTH + 1):
+                if model[vertical_index] > 0:
+                    grid[row_index][j] = "|    "
+                else:
+                    grid[row_index][j] = "     "
+                vertical_index += 1
+        
 
-    # decode the meaning of the assignment
-    for t in range(MAKESPAN+1):
-        print("Timestep " + str(t) + ":")
-        for p in range(NR_TILES):
-            for n in range(NR_TILES):
-                if model[pos_to_atID(n,p,t) - 1] > 0:
-                    print(n, end=" ")
-            if (p+1) % GRID_SIZE == 0:
-                print()
-        print()
+    for i, row in enumerate(grid, 1):
+        # Join the row contents into a single line string
+        row_str = ' '.join(map(str, row))
+        
+        # If the row index is odd (1-based), print it three times
+        if i % 2 == 0:
+            print(row_str)
+            print(row_str)
+            print(row_str)
+        else:
+            print(row_str)
 
 if __name__ == "__main__":
 
